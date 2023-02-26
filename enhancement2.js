a9ffbb449fdc005aa8f1ed48619c79a6a29d5d4b
Adithya0707 <53269613+Adithya0707@users.noreply.github.com>
2023-02-26 13:13:30 +0530

    Add files via upload

--- enhancement2.js
+++ enhancement2.js
@@ -11,49 +11,217 @@ let tms001 = [];
 let PSMVal = [2, 5, 10, 15, 20];
 
 $("#submitbtn").click(function () {
-  let tmsAll = [];
-  tmsAll = $("#TMS").val();
-  for (let r = 0; r < PSMVal.length; r++) {
-    let pms = PSMVal[r];
+  if (document.querySelector("#IDMT").value == 1.3) {
+    document.querySelector("#CurveName").textContent = "Curve Type : 1.3 sec";
+    let tmsAll = [];
+    tmsAll = $("#TMS").val();
+
     for (let t = 0; t < tmsAll.length; t++) {
       const t1ms = tmsAll[t];
+      for (let r = 0; r < PSMVal.length; r++) {
+        let pms = PSMVal[r];
+        const calcT = (1.3 / (Math.log(pms) / Math.LN10)) * t1ms;
+        X = Number(calcT.toFixed(2));
+        if (X < 0.03) {
+          document.querySelector("#T").textContent = 0.03;
+          console.log("You have reached minimum operating range of the relay");
+        } else {
+          document.querySelector("#T").textContent = X;
+        }
 
-      const calcT = (1.3 / (Math.log(pms) / Math.LN10)) * t1ms;
-      X = Number(calcT.toFixed(2));
-      if (X < 0.03) {
-        document.querySelector("#T").textContent = 0.03;
-        console.log("You have reached minimum operating range of the relay");
-      } else {
-        document.querySelector("#T").textContent = X;
-      }
+        $("#table1").append(
+          "<tr><td>" +
+            t1ms +
+            "</td><td>" +
+            pms +
+            "</td><td>" +
+            Number($("#T").text()) +
+            "</td></tr>"
+        );
 
-      $("#table1").append(
-        "<tr><td>" +
-          t1ms +
-          "</td><td>" +
-          pms +
-          "</td><td>" +
-          Number($("#T").text()) +
-          "</td></tr>"
-      );
-
-      if (tmsAll[t] == "1") {
-        tms1.push(Number(document.querySelector("#T").textContent));
-        console.log(tms1);
-      } else if (tmsAll[t] == "1.1") {
-        tms11.push(Number(document.querySelector("#T").textContent));
-        console.log(tms11);
-      } else if (tmsAll[t] == "0.1") {
-        tms01.push(Number(document.querySelector("#T").textContent));
-        console.log(tms01);
-      } else if (tmsAll[t] == "0.01") {
-        tms001.push(Number(document.querySelector("#T").textContent));
-        console.log(tms001);
-      } else if (tmsAll[t] == "2") {
-        tms2.push(Number(document.querySelector("#T").textContent));
-        console.log(tms2);
+        if (tmsAll[t] == "1") {
+          tms1.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "1.1") {
+          tms11.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "0.1") {
+          tms01.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "0.01") {
+          tms001.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "2") {
+          tms2.push(Number(document.querySelector("#T").textContent));
+        }
       }
     }
+  } else if (document.querySelector("#IDMT").value == "NI") {
+    document.querySelector("#CurveName").textContent =
+      "Curve Type : Normal nverse";
+    let tmsAll = [];
+    tmsAll = $("#TMS").val();
+
+    for (let t = 0; t < tmsAll.length; t++) {
+      const t1ms = tmsAll[t];
+      for (let r = 0; r < PSMVal.length; r++) {
+        let pms = PSMVal[r];
+        const calcT = (0.14 / (Math.pow(pms, 0.02) - 1)) * t1ms; //NI
+        X = Number(calcT.toFixed(2));
+        if (X < 0.03) {
+          document.querySelector("#T").textContent = 0.03;
+          console.log("You have reached minimum operating range of the relay");
+        } else {
+          document.querySelector("#T").textContent = X;
+        }
+
+        $("#table1").append(
+          "<tr><td>" +
+            t1ms +
+            "</td><td>" +
+            pms +
+            "</td><td>" +
+            Number($("#T").text()) +
+            "</td></tr>"
+        );
+
+        if (tmsAll[t] == "1") {
+          tms1.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "1.1") {
+          tms11.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "0.1") {
+          tms01.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "0.01") {
+          tms001.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "2") {
+          tms2.push(Number(document.querySelector("#T").textContent));
+        }
+      }
+    }
+  } else if (document.querySelector("#IDMT").value == "VI") {
+    document.querySelector("#CurveName").textContent =
+      "Curve Type : Very Inverse";
+    let tmsAll = [];
+    tmsAll = $("#TMS").val();
+
+    for (let t = 0; t < tmsAll.length; t++) {
+      const t1ms = tmsAll[t];
+      for (let r = 0; r < PSMVal.length; r++) {
+        let pms = PSMVal[r];
+        const calcT = (13.5 / (pms - 1)) * t1ms; //VI
+
+        X = Number(calcT.toFixed(2));
+        if (X < 0.03) {
+          document.querySelector("#T").textContent = 0.03;
+          console.log("You have reached minimum operating range of the relay");
+        } else {
+          document.querySelector("#T").textContent = X;
+        }
+
+        $("#table1").append(
+          "<tr><td>" +
+            t1ms +
+            "</td><td>" +
+            pms +
+            "</td><td>" +
+            Number($("#T").text()) +
+            "</td></tr>"
+        );
+
+        if (tmsAll[t] == "1") {
+          tms1.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "1.1") {
+          tms11.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "0.1") {
+          tms01.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "0.01") {
+          tms001.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "2") {
+          tms2.push(Number(document.querySelector("#T").textContent));
+        }
+      }
+    }
+  } else if (document.querySelector("#IDMT").value == "EI") {
+    document.querySelector("#CurveName").textContent =
+      "Curve Type : Extremely Inverse";
+    let tmsAll = [];
+    tmsAll = $("#TMS").val();
+
+    for (let t = 0; t < tmsAll.length; t++) {
+      const t1ms = tmsAll[t];
+      for (let r = 0; r < PSMVal.length; r++) {
+        let pms = PSMVal[r];
+        const calcT = (80 / (Math.pow(pms, 2) - 1)) * t1ms; //EI
+        X = Number(calcT.toFixed(2));
+        if (X < 0.03) {
+          document.querySelector("#T").textContent = 0.03;
+          console.log("You have reached minimum operating range of the relay");
+        } else {
+          document.querySelector("#T").textContent = X;
+        }
+
+        $("#table1").append(
+          "<tr><td>" +
+            t1ms +
+            "</td><td>" +
+            pms +
+            "</td><td>" +
+            Number($("#T").text()) +
+            "</td></tr>"
+        );
+
+        if (tmsAll[t] == "1") {
+          tms1.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "1.1") {
+          tms11.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "0.1") {
+          tms01.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "0.01") {
+          tms001.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "2") {
+          tms2.push(Number(document.querySelector("#T").textContent));
+        }
+      }
+    }
+  } else if (document.querySelector("#IDMT").value == "0.6") {
+    document.querySelector("#CurveName").textContent = "Curve Type : 0.6 Sec";
+    let tmsAll = [];
+    tmsAll = $("#TMS").val();
+
+    for (let t = 0; t < tmsAll.length; t++) {
+      const t1ms = tmsAll[t];
+      for (let r = 0; r < PSMVal.length; r++) {
+        let pms = PSMVal[r];
+        const calcT = (0.6 / (Math.log(pms) / Math.LN10)) * t1ms; //0.6
+        X = Number(calcT.toFixed(2));
+        if (X < 0.03) {
+          document.querySelector("#T").textContent = 0.03;
+          console.log("You have reached minimum operating range of the relay");
+        } else {
+          document.querySelector("#T").textContent = X;
+        }
+
+        $("#table1").append(
+          "<tr><td>" +
+            t1ms +
+            "</td><td>" +
+            pms +
+            "</td><td>" +
+            Number($("#T").text()) +
+            "</td></tr>"
+        );
+
+        if (tmsAll[t] == "1") {
+          tms1.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "1.1") {
+          tms11.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "0.1") {
+          tms01.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "0.01") {
+          tms001.push(Number(document.querySelector("#T").textContent));
+        } else if (tmsAll[t] == "2") {
+          tms2.push(Number(document.querySelector("#T").textContent));
+        }
+      }
+    }
+  } else {
+    return;
   }
 });
 
@@ -65,7 +233,7 @@ function curveGenerator() {
   //Generate graph
   $("#graph").highcharts({
     title: {
-      text: "IDMT Curves",
+      text: "",
     },
     xAxis: {
       categories: PSMVal,
@@ -131,8 +299,10 @@ function Convert_HTML_To_PDF() {
   const element = document.getElementById("fullPage");
   //document.getElementById("Product").style.display = "none";
   $("#Product").remove();
-  document.getElementById("IDMT").style.borderColor = "red";
+  //$("#Product").remove();
 
+  //document.getElementById("Product").style.borderColor = "red";
+  $("#IDMT").remove();
   //document.getElementById("TMS").style.display = "none";
   $("#TMS").remove();
   //document.getElementById("submitbtn").style.display = "none";
@@ -147,7 +317,18 @@ function Convert_HTML_To_PDF() {
   $("#T").remove();
   //document.getElementById("labelselect").style.display = "none";
   $("#labelselect").remove();
-  document.getElementById("graph").style.marginTop = "100px";
+  //$("#navbar").remove();
+  $("#CurveName").show();
+  //$("#header").show();
+  $("#reporttop").show();
+  $("#report").show();
+  $("#line1").show();
+  $("#line2").show();
+  $("#relayname").show();
+
+  document.getElementById("CurveName").style.marginTop = "40px";
+  //document.getElementById("graph").style.marginTop = "90px";
+
   document.getElementById("table1").style.marginTop = "0px";
   $("#tableoutput").remove();
   $(".bootstrap-select").remove();
@@ -160,3 +341,12 @@ function fun1() {
   saveAsExcel("table1", "table.xlsx");
   Convert_HTML_To_PDF();
 }
+
+$(window).ready(function () {
+  $("#relayname").hide();
+  $("#reporttop").hide();
+  $("#report").hide();
+  $("#line1").hide();
+  $("#line2").hide();
+  $("#productIMG").show();
+});
